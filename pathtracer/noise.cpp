4ae0e37febcaaec2a213e596f5c1851275bf7c9a
Alotte <alma.ottedag@gmail.com>
2017-04-11 18:27:36 +0200

    Moved comments because that's nice

--- pathtracer/noise.cpp
+++ pathtracer/noise.cpp
@@ -98,17 +98,20 @@ public:
 
 
 	double perlin(double x, double y, double z) {
-		if (repeat > 0) {									// If we have any repeat on, change the coordinates to their "local" repetitions
+		// If we have any repeat on, change the coordinates to their "local" repetitions
+		if (repeat > 0) {									
 			x = fmod(x,repeat);
 			y = fmod(y , repeat);
 			z = fmod(z , repeat);
 		}
-
-		int xi = abs((int)x & 255);								// Calculate the "unit cube" that the point asked will be located in
-		int yi = abs((int)y & 255);								// The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
+		// Calculate the "unit cube" that the point asked will be located in
+		// The left bound is ( |_x_|,|_y_|,|_z_| ) and the right bound is that
+		int xi = abs((int)x & 255);								
+		int yi = abs((int)y & 255);							
 		int zi = abs((int)z & 255);									
-															// plus 1.  Next we calculate the location (from 0.0 to 1.0) in that cube.
-		double xf = x - (int)x;								// We also fade the location to smooth the result.
+		// plus 1.  Next we calculate the location (from 0.0 to 1.0) in that cube.
+		// We also fade the location to smooth the result.									
+		double xf = x - (int)x;								
 		double yf = y - (int)y;
 		double zf = z - (int)z;
 		
@@ -129,11 +132,17 @@ public:
 		bbb = p[p[p[inc(xi)] + inc(yi)] + inc(zi)];
 
 		double x1, x2, y1, y2;
-		x1 = lerp(grad(aaa, xf, yf, zf),				// The gradient function calculates the dot product between a pseudorandom
-			grad(baa, xf - 1, yf, zf),				// gradient vector and the vector from the input coordinate to the 8
-			u);										// surrounding points in its unit cube.
-		x2 = lerp(grad(aba, xf, yf - 1, zf),				// This is all then lerped together as a sort of weighted average based on the faded (u,v,w)
-			grad(bba, xf - 1, yf - 1, zf),				// values we made earlier.
+		// The gradient function calculates the dot product between a pseudorandom	
+		// gradient vector and the vector from the input coordinate to the 8	
+		// surrounding points in its unit cube.
+		// This is all then lerped together as a sort of weighted average based on the faded (u,v,w)
+		// values we made earlier.			
+		x1 = lerp(grad(aaa, xf, yf, zf),			
+			grad(baa, xf - 1, yf, zf),				
+			u);		
+
+		x2 = lerp(grad(aba, xf, yf - 1, zf),			
+			grad(bba, xf - 1, yf - 1, zf),				
 			u);
 		y1 = lerp(x1, x2, v);
 
@@ -144,91 +153,7 @@ public:
 			grad(bbb, xf - 1, yf - 1, zf - 1),
 			u);
 		y2 = lerp(x1, x2, v);
-
-		return (lerp(y1, y2, w) + 1) / 2;						// For convenience we bound it to 0 - 1 (theoretical min/max before is -1 - 1)
+		// For convenience we bound it to 0 - 1 (theoretical min/max before is -1 - 1)
+		return (lerp(y1, y2, w) + 1) / 2;						
 	}
-
-
-
-	/*
-	////////////////////////////////////////////////////////////////////////
-	Noises
-	////////////////////////////////////////////////////////////////////////
-	/*
-	double perlin(double x, double y, double z) {
-		if (repeat > 0) {
-			printf("WTF.\n");
-			x = fmod(x, repeat); //TODO: doubleing point modulo, hopefully works.
-			y = fmod(y, repeat);
-			z = fmod(z, repeat);
-		}
-		
-		int xi = (int) x & 255; // Bitwise and of floored to find cube 
-		int yi = (int) y & 255; // within [0,255] surrounding the point
-		int zi = (int) z & 255;
-
-		double xf = abs(x - (int)x); // Transform point to cube space
-		double yf = abs(y - (int)y);
-		double zf = abs(z - (int)z);
-
-		double u = fade(xf); // faded cube space coordinates of point
-		double v = fade(yf);
-		double w = fade(zf);
-
-		//perlin gradient values in the unit cube around a point. 
-		//Assigned values by hash
-		int aaa, aba, aab, abb, baa, bba, bab, bbb;
-
-		//Hashes the eight unit cube coordinates based on our input.
-		aaa = p[p[p[xi] + yi] + zi];
-		aba = p[p[p[xi] + inc(yi)] + zi];
-		aab = p[p[p[xi] + yi] + inc(zi)];
-		abb = p[p[p[xi] + inc(yi)] + inc(zi)];
-
-		baa = p[p[p[inc(xi)] + yi] + zi];
-		bba = p[p[p[inc(xi)] + inc(yi)] + zi];
-		bab = p[p[p[inc(xi)] + yi] + inc(zi)];
-		bbb = p[p[p[inc(xi)] + inc(yi)] + inc(zi)];
-
-		//printf("\n abb: %f, bbb %f", abb, bbb);
-
-		
-		//Interpolate between corner gradients and our point
-		
-		double x1, x2, y1, y2;
-		x1 = lerp(
-			grad(aaa, xf, yf, zf), //the modified x-y-z make up the 
-			grad(baa, xf - 1, yf, zf), //displacement vector for the
-			u						// corresponding cube vertex.
-		);
-		//printf("x1 first: %f", x1);
-
-		x2 = lerp(
-			grad(aba, xf, yf - 1, zf),
-			grad(bba, xf - 1, yf - 1, zf),
-			u
-		);
-
-		y1 = lerp(x1, x2, v);
-
-		x1 = lerp(							// we rewrite the x1-2 in our other corners.
-			grad(aab, xf, yf, zf - 1),		//to get y2.
-			grad(bab, xf - 1, yf, zf - 1),
-			u
-		);
-
-		x2 = lerp(
-			grad(abb, xf, yf - 1, zf - 1),
-			grad(bbb, xf - 1, yf - 1, zf - 1),
-			u
-		);
-
-		y2 = lerp(x1, x2, v);
-
-		return (lerp(y1, y2, w) + 1) / 2; // make sure result within 0-1
-	}
-	*/
-
-
-
 };
